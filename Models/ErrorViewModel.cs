b88f7a00b2efb3f3be82a08763b0c9849d5513da
Moises Navarro <moises@vivogaming.com>
2022-10-02 15:18:44 -0600

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoI_MoisesNavarro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
